184072f43023a2fe1c853f80cf9b3f5789127bf0
OKB19 <130286251+OKB19@users.noreply.github.com>
2023-09-12 18:32:41 +0530

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("Digit-recognition Images")
 
 st.write("Predict the sport that is being represented in the image.")
 
-model = load_model("model.h5")
+model = load_model(".model.h5")
 labels = {
         0: '0',
         1: '1',
@@ -38,7 +38,7 @@ uploaded_file = st.file_uploader(
 )
 predictions=-1
 if uploaded_file is not None:
-    image1 = Image.open("/content/7.jpg")
+    image1 = Image.open("image1.jpg")
     image1=image.smart_resize(image1,(28,28))
     img_array = image.img_to_array(image1)
     img_array = np.expand_dims(img_array, axis=0)
@@ -50,7 +50,7 @@ if uploaded_file is not None:
 st.write("### Prediction Result")
 if st.button("Predict"):
     if uploaded_file is not None:
-        image1 = Image.open("/content/7.jpg")
+        image1 = Image.open("image2.jpg")
         st.image(image1, caption="Uploaded Image", use_column_width=True)
         st.markdown(
             f"<h2 style='text-align: center;'>Image of {label}</h2>",
@@ -64,14 +64,14 @@ st.write("If you would not like to upload an image, you can use the sample image
 sample_img_choice = st.button("Use Sample Image")
 
 if sample_img_choice:
-    image1 = Image.open("/content/7.jpg")
+    image1 = Image.open("image3.jpg")
     image1=image.smart_resize(image1,(28,28))
     img_array = image.img_to_array(image1)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = img_array/255.0
     predictions = model.predict(img_array)
     label=labels[np.argmax(predictions)]
-    image1 = Image.open("/content/7.jpg")
+    image1 = Image.open("image4.jpg")
     st.image(image1, caption="Uploaded Image", use_column_width=True)    
     st.markdown(
         f"<h2 style='text-align: center;'>{label}</h2>",
